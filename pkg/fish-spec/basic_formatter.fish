121e966aaf9165cb29b4ff255a926a514f5215ad
Bruno Pinto <brunoferreirapinto@gmail.com>
2015-12-19 12:51:44 +0000

    Improve failure reports
    Show name of the test that failed with the failure message.

--- pkg/fish-spec/basic_formatter.fish
+++ pkg/fish-spec/basic_formatter.fish
@@ -12,6 +12,8 @@ function __fish-spec.all_specs_finished -e all_specs_finished -a spec
 end
 
 function __fish-spec.spec_init -e spec_init -a spec
+  set -g __current_spec_name (echo $spec | sed 's/^[0-9]*_//;s/_/ /g;s/^it/It/')
+  set -e __current_spec_output
   set -e __current_spec_status
 end
 
@@ -33,7 +35,9 @@ function __fish-spec.spec_success -e spec_success
 end
 
 function __fish-spec.spec_error -e spec_error
-  echo -n 'F'
+  echo -e "\n\nFailure: $__current_spec_name"
+  echo (omf::em)  $__current_spec_output(omf::off)
+
   set -g __any_spec_failed true
 end
 
@@ -47,8 +51,8 @@ end
 
 function __fish-spec_assertion_error -e assertion_error -a error_message
   # Mimics output redirect inside an event handler
-  if set -q __fish_spec_output
-    set __fish_spec_output $error_message
+  if set -q __current_spec_quiet
+    set -g __current_spec_output $error_message
   else
     echo $error_message
   end
