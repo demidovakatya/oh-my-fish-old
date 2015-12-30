208da4c7710b937fc95c7a48cce1efcae4f95326
Derek Stavis <dekestavis@gmail.com>
2015-12-30 01:53:58 -0200

    pkg/omf: Move color functions and status variables to init.fish

--- pkg/omf/omf.fish
+++ pkg/omf/omf.fish
@@ -8,30 +8,6 @@
 #   Provides options to list, download and remove packages, update
 #   the framework, create / submit a new package, etc.
 
-set -g OMF_MISSING_ARG   1
-set -g OMF_UNKNOWN_OPT   2
-set -g OMF_INVALID_ARG   3
-set -g OMF_UNKNOWN_ERR   4
-
-function omf::em
-  set_color $fish_color_match ^/dev/null; or set_color cyan
-end
-
-function omf::dim
-  set_color $fish_color_autosuggestion ^/dev/null; or set_color 555
-end
-
-function omf::under
-  set_color --underline
-end
-
-function omf::err
-  set_color $fish_color_error ^/dev/null; or set_color red --bold
-end
-
-function omf::off
-  set_color normal
-end
 
 function omf -d "Oh My Fish"
   if test (count $argv) -eq 0
